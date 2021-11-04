674d07b1ebcaa20629ded455016b001e8f6c3082
Mikaela Allan <me@mikaela.ca>
2021-11-04 18:17:26 -0400

    create formula for serve-d@v0.6.0

---
+++ Formula/serve-d.rb
@@ -0,0 +1,21 @@
+class ServeD < Formula
+  desc "An LSP server for the D programming language"
+  homepage "https://github.com/Pure-D/serve-d"
+  url "https://github.com/Pure-D/serve-d.git",
+      tag: "v0.6.0",
+      revision: "a3c0469e0332c48057e5ed93a232af5f5f7ef250" 
+  license "MIT"
+  head "https://github.com/Pure-D/serve-d.git", branch: "master"
+
+  depends_on "dub" => :build
+  depends_on "ldc" => :build
+
+  def install
+    system "dub", "build", "--compiler=ldc2", "--build=release"
+    bin.install "serve-d"
+  end
+
+  test do
+    system "#{bin}/serve-d", "--version"
+  end
+end
